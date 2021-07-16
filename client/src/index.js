ea38ed241b7ea40838f6cc897fc938cb313e5c0e
Brian Schaaf <bschaaf1017@gmail.com>
2021-07-16 14:07:15 -0400

    all deps installed webpack configured and express serving react app

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppMain from './Components/AppMain'
+
+// export default function App(): JSX.Element {
+//     return (
+//         <div>
+//             <h1>Welcome</h1>
+//         </div>
+//     )
+// }
+
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<AppMain/>, root);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const OUT_DIR = path.resolve(__dirname, 'public');
+
+const rules = [
+    {
+        test: /\.(tsx|ts|js|jsx)/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    },
+    {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: true,
+                },
+            },
+        ],
+    },
+]
+
+module.exports = {
+    target: 'web',
+    mode: 'development',
+    entry:  './client/src/index.js',
+    output:{
+        path: OUT_DIR,
+        filename: 'bundle.js'
+    },
+    module: { rules },
+    resolve: {extensions: ['.js', '.jsx', '.tsx', '.ts']}
+}
