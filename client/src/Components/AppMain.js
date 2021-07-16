8519be4fe39369b341bc9bd865e29c5e8884abb6
Brian Schaaf <bschaaf1017@gmail.com>
2021-07-16 14:40:18 -0400

    sekeleton for NavBar Built

--- client/src/Components/AppMain.js
+++ client/src/Components/AppMain.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import NavBar from './Common/NavBar';
+
 export default function AppMain() {
     return (
         <div>
-            LEarnatric
+            <NavBar />
         </div>
     )
 }
--- client/src/Components/Common/NavBar.js
+++ client/src/Components/Common/NavBar.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+
+import { StyleSheet, css } from 'aphrodite';
+
+import logo from './Images/LearnAtric.png';
 
 export default function NavBar() {
     return (
-        <div>
-            
+        <div className={css(styles.MainContainer)}>
+            <img src={logo} />
+            <div className={css(styles.LoginButton)}>
+                <div className={css(styles.ButtonText)}></div>
+            </div>
+            <div className={css(styles.SignUpButton)}>
+                <div className={css(styles.ButtonText)}></div>
+            </div>
+
         </div>
     )
 }
+
+const styles = StyleSheet.create({
+    MainContainer: {
+        display: 'flex',
+        height: '60px',
+        width: '100%',
+        justifyContent: 'space-evenly'
+    },
+    LoginButton: {
+        border: '1px solid',
+        borderRadius: '1px'
+    },
+    SignUpButton: { 
+        border: '1px solid',
+        borderRadius: '1px'
+    },
+    ButtonText: { 
+
+    }
+})
