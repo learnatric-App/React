1e8e6d88acc5366bf7e7e05a02f569a6755f1aa3
Brian Schaaf <bschaaf1017@gmail.com>
2021-07-19 15:31:25 -0400

    removed redux deps and files, hooked up context API

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import {Provider} from "react-redux";
 
 import AppMain from './Components/AppMain';
-import store from './store.js';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
-    <Provider store={store}>
         <Router>
             <AppMain/> 
-        </Router>
-    </Provider>,
+        </Router>,
     root
 );
