7339b4ffbb6328e40c547e03946218ed5de18b38
Brian Schaaf <bschaaf1017@gmail.com>
2021-07-25 23:39:55 -0400

    added react custom checkbox lib and styled checkbox to mnatch page

--- client/src/Components/SignUp/CreateChildAccount/FullExperience.js
+++ client/src/Components/SignUp/CreateChildAccount/FullExperience.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import React, { useState } from 'react';
+import { StyleSheet, css } from 'aphrodite-jss';
 import { useForm } from 'react-hook-form';
+import Checkbox from "react-custom-checkbox";
+
+import { FcCheckmark } from 'react-icons/fc'
 
 
-const bodyText = 'Our curriculum is based on the US Federal Core Standards and was created from scratch by a team of committed teachers. We would love to continue our strong alignment with teachers by keeping your child’s teacher notified of your child’s progress.'
+const bodyText = 'Our curriculum is based on the US Federal Core Standards and was created from scratch by a team of committed teachers. We would love to continue our strong alignment with teachers by keeping your child’s teacher notified of your child’s progress.';
+const checkBoxText = "I am okay with Learnatric emailing my child’s teacher periodically about academic progress.";
+
 
 export default function FullExperience() {
+
+    const [isChecked, setIsChecked] = useState(false);
 
     const { register, handleSubmit, getValues, formState: { errors } } = useForm();
 
@@ -24,10 +31,45 @@ export default function FullExperience() {
                     </div>
                 </div>
                 <div className={css(styles.FormContainer)}>
-                    <input type="text" placeholder="School District " {...register("School District ", {})} />
-                    <input type="text" placeholder="School" {...register("School", {})} />
-                    <input type="text" placeholder="Teachers name" {...register("Teachers name", {})} />
-                    <input type="email" placeholder="Teachers email" {...register("Teachers email", {})} />
+                    <div className={css(styles.SideBySideContainer)}>
+                        <input 
+                            className={css(styles.InputContainer)}
+                            type="text" 
+                            placeholder="School District " 
+                            {...register("School District ", {})} />
+                        <input 
+                            className={css(styles.InputContainer)}
+                            type="text" 
+                            placeholder="School" 
+                            {...register("School", {})} />
+                    </div>
+                    <div className={css(styles.SideBySideContainer)}>
+                        <input 
+                            className={css(styles.InputContainer)}
+                            type="text" 
+                            placeholder="Teachers name" 
+                            {...register("Teachers name", {})} />
+                        <input 
+                            className={css(styles.InputContainer)}
+                            type="email" 
+                            placeholder="Teachers email" 
+                            {...register("Teachers email", {})} />
+                    </div>
+                    <div className={css(styles.CheckboxContainer)}>
+                        <Checkbox 
+                            checked={isChecked}
+                            icon={<FcCheckmark size={50}/>}
+                            name="my-input"
+                            onChange={(e) => setIsChecked(true)}
+                            borderRadius={50}
+                            borderColor={'black'}
+                            borderWidth={3}
+                            className={css(styles.CheckBox)}
+                            size={30}
+                            label={checkBoxText}
+                            labelStyle={{ marginLeft: 5, userSelect: "none", fontSize: '18px' }}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
@@ -88,5 +130,40 @@ const styles = StyleSheet.create({
     },
     StaticBodyText: {
 
+    },
+    FormContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '90%',
+        marginLeft: '3em',
+        
+    },
+    SideBySideContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '75%', 
+        marginTop: '1em',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    InputContainer: {
+        fontSize: '18px',
+        height: '35px',
+        width: '45%',
+        marginTop: 'auto',
+        borderRadius: '10px',
+        boxSizing: 'border-box',
+        boxShadow: '0px 4px 4px rgba(0,0,0,0.25)'
+    },
+    CheckboxContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '1em',
+    },
+    CheckBox: {
+        cursor: 'pointer',
+        backgroundColor: '#faeaa7',
     }
 })
